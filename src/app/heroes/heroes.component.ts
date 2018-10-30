f48f7becaf98a366c5b287962b16e52d74c1b185
‘Ignacio’ <royovillanova.i@gmail.com>
2018-10-30 14:22:19 +0100

    service terminado

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -18,7 +18,8 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 
   onSelect(hero: Hero): void {
